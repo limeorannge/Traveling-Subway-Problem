ea5e2e85d260d0e633f74f8cf1f28cb61df3b537
limeorannge <insoo0327@snu.ac.kr>
2023-01-04 08:14:11 +0900

    first commit

--- data_former.py
+++ data_former.py
@@ -5,8 +5,8 @@ with open("ahfinished.json", "r", encoding='utf-8') as f:
     json_data = json.load(f)
 
 a = json_data.keys()
-id = dict()
-idr = dict()
+id = dict() # corresponds to each station
+idr = dict() # reverse of id
 cnt = 0
 for i in a:
     if i.split()[1] not in passing_lines:
@@ -17,13 +17,29 @@ lines = set()
 for i in a:
     if i.split()[1] not in passing_lines:
         lines.add(i.split()[1])
-print(len(lines))
-edges = []
+lid = dict() # corresponds to each lane
+lidr = dict() # reverse of lid
+cnt = 0
+for i in lines:
+    if i not in passing_lines:
+        lid[i] = cnt
+        lidr[cnt] = i
+        cnt+=1
+station_data = [] # contains data of stations in tuple list form
+for i in a:
+    if i.split()[1] not in passing_lines:
+        station_data.append((id[i], lid[i.split()[1]]))
+print(len(station_data))
+for i in station_data:
+    print(*i)        
+
+edges = [] # contains data of the path between each stations in tuple list form
 for i in a:
     for j in json_data[i]:
         try:
             edges.append((id[i], id[j[0]], j[2] if j[2]!='transfer' else 0))
         except:
             pass
+print(len(edges))
 for i in edges:
     print(*i)
