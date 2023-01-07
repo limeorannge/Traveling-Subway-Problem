0c81455e4ca251c14e780f35029eef37e4d145cb
limeorannge <insoo0327@snu.ac.kr>
2023-01-07 21:41:59 +0900

    solver.cpp algorithm part complete(not debugged)

--- solver.cpp
+++ solver.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <stdlib.h>
+#include <string.h>
 using namespace std;
 typedef struct Edge{
     int e, c;
 }Edge;
 typedef struct Node{
-    int now, stat;
+    int now, stat, dist;
 }Node;
 int line[258];
 bool vis[258][1<<20];
@@ -15,9 +17,31 @@ int ans = 1000000007;
 queue<Node> q;
 void bfs(int s){
     vis[s][1<<line[s]] = 1;
-    q.push({s, 1<<line[s]});
+    q.push({s, 1<<line[s], 0});
     while(!q.empty()){
-        
+        Node curr = q.front();
+        q.pop();
+        if(curr.stat == (1<<20)-1){ //checking if every lane is visited
+            ans = min(ans, curr.dist);
+            continue;
+        } 
+        for(auto i:map[curr.now]){
+            if(line[i.e] != line[curr.now]){ // trying to transfer
+                if(!(curr.stat & (1<<line[i.e]))){ // if the lane is not visited 
+                    vis[i.e][curr.stat | (1<<line[i.e])] = 1;
+                    q.push({i.e, curr.stat | (1<<line[i.e]), curr.dist});
+                }
+            }
+            else{ // trying to go
+                if(!vis[i.e][curr.stat]){
+                    if(curr.dist + i.c > ans){
+                        continue;
+                    }
+                    vis[i.e][curr.stat] = 1;
+                    q.push({i.e, curr.stat, curr.dist + i.c});
+                }
+            }
+        }
     }
 }
 int main(){
@@ -34,5 +58,8 @@ int main(){
         cin >> s >> e >> c;
         map[s].push_back({e, c});
     }
-    for(int i=0; i<258; i++) bfs(i);
+    for(int i=0; i<258; i++){
+        memset(vis, 0, sizeof(vis));
+        bfs(i);
+    }
 }
