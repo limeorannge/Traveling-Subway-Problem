593ef38c4d9d685160953854c2d7264f588b5eaa
Insoo <insoo0327@snu.ac.kr>
2026-01-31 06:45:43 +0900

    fixed logic error

--- README.md
+++ README.md
@@ -27,6 +27,6 @@ Reproducing the repo's reported result
 - The `README.md` previously contained a final result example. To reproduce the same output: generate the data, build, run, then pretty-print as shown above. The solver prints progress messages as it simulates different starting points.
 
 ## Final result
-Path with shortest time was the one with starting point "Incheon City Hall" station. It requires 120 minutes to travel around whole metro system.
+Path with shortest time was the one with starting point "Geomam" station. It requires 195 minutes to travel around whole metro system.
 Full path is:
-> 인천시청역 인천2호선 --> 인천시청역 인천1호선 --> 계양역 공항철도 --> 서울역 4호선 --> 성신여대입구역 우이신설선 --> 보문역 6호선 --> 공덕역 5호선 --> 여의도역 9호선 --> 샛강역 신림선 --> 대방역 1호선 --> 청량리역 경의중 앙선 --> 상봉역 경춘선 --> 상봉역 7호선 --> 건대입구역 2호선 --> 교대역 3호선 --> 양재역 신분당선 --> 판교역 경강선 --> 이매역 수인분당선 --> 모란역 8호선
+> 검암역 인천2호선, 인천시청역 인천1호선, 부평구청역 7호선, 보라매역 신림선, 샛강역 9호선, 신논현역 신분당선, 판교역 경강선, 이매역 수인분당선, 수서역 3호선, 가락시장역 8호선, 잠실역 2호선, 왕십리역 경의중앙선, 청량리역 경춘선, 청량리역 1호선, 신설동역 우이신설선, 보문역 6호선, 청구역 5호선, 동대문역사문화공원역 4호선, 서울역 공항철도, 검암역 인천2호선
--- src/solver.cpp
+++ src/solver.cpp
@@ -6,94 +6,154 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stack>
+#include <algorithm> 
+
 using namespace std;
+
+
 typedef struct Edge{
-    int e, c;
-}Edge;
+    int e, c; 
+} Edge;
+
+
 typedef struct Node{
-    int now, stat, dist;
-}Node;
-void byte_encoder(int x){
-    for(int i=0; i<19; i++){
-        if(x&(1<<i)){
-            cout << "line " << i << ", ";
-        }
+    int now; 
+    int stat; 
+    int dist;
+    
+
+    int prev_now;   
+    int prev_stat;
+
+
+    bool operator>(const Node& other) const {
+        return dist > other.dist;
     }
-    cout<<"visited" << ln;
-}
+} Node;
+
 int ans_starting_point;
 int line[258];
 bool vis[258][1<<19];
-Edge track[258][1<<19];
+Edge track[258][1<<19]; 
 vector<Edge> map[258];
+
 int starting_point;
-int ans = 190;
+int ans = 2000000000; 
 Edge st;
-queue<Node> q;
-void bfs(int s){
-    vis[s][1<<line[s]] = 1;
-    q.push({s, 1<<line[s], 0});
-    while(!q.empty()){
-        Node curr = q.front();
-        q.pop();
-        //cout << curr.now << rp << curr.dist << ln;
-        //byte_encoder(curr.stat);
-        if(curr.stat == (1<<19)-1){ //checking if every lane is visited
-            ans = min(ans, curr.dist);
-            ans_starting_point = starting_point;
-            st = {curr.now, curr.stat};
-            continue;
-        } 
-        for(auto i:map[curr.now]){
-            if(line[i.e] != line[curr.now]){ // trying to transfer
-                if(!(curr.stat & (1<<line[i.e]))){ // if the lane is not visited 
-                    vis[i.e][curr.stat | (1<<line[i.e])] = 1;
-                    track[i.e][curr.stat | (1<<line[i.e])] = {curr.now, curr.stat};
-                    q.push({i.e, curr.stat | (1<<line[i.e]), curr.dist});
+
+priority_queue<Node, vector<Node>, greater<Node>> pq;
+
+void dijkstra(int s){
+    while(!pq.empty()) pq.pop();
+
+    pq.push({s, 1<<line[s], 0, -1, 0});
+    
+    while(!pq.empty()){
+        Node curr = pq.top();
+        pq.pop();
+
+        if(vis[curr.now][curr.stat]) continue;
+        vis[curr.now][curr.stat] = true;
+
+        if(curr.prev_now != -1) {
+            track[curr.now][curr.stat] = {curr.prev_now, curr.prev_stat};
+        }
+
+        if(curr.stat == (1<<19)-1) {
+            if(curr.now == starting_point) {
+                if(curr.dist < ans) {
+                    ans = curr.dist;
+                    ans_starting_point = starting_point;
+                    st = {curr.now, curr.stat};
+                }
+                return; 
+            }
+        }
+
+        if(curr.dist >= ans) continue;
+
+        for(auto i : map[curr.now]){
+            int next_stat = curr.stat;
+            int cost = i.c;
+
+            if(line[i.e] != line[curr.now]){
+                bool isNew = !(curr.stat & (1<<line[i.e]));
+                bool isReturnHome = (curr.stat == ((1<<19)-1)) && (line[i.e] == line[starting_point]);
+
+                if(isNew || isReturnHome){
+                    next_stat = curr.stat | (1<<line[i.e]);
+                    
+                    if(!vis[i.e][next_stat]){
+                        pq.push({i.e, next_stat, curr.dist + cost, curr.now, curr.stat});
+                    }
                 }
             }
-            else{ // trying to go
-                if(!vis[i.e][curr.stat]){
-                    if(curr.dist + i.c > ans){
-                        continue;
-                    }
-                    vis[i.e][curr.stat] = 1;
-                    track[i.e][curr.stat] = {curr.now, curr.stat};
-                    q.push({i.e, curr.stat, curr.dist + i.c});
+            else{ 
+                if(!vis[i.e][next_stat]){
+                    pq.push({i.e, next_stat, curr.dist + cost, curr.now, curr.stat});
                 }
             }
         }
     }
 }
+
 int main(){
-    freopen("../data/data.txt", "r", stdin);
-    //freopen("result.txt", "w", stdout);
+    if(freopen("../data/data.txt", "r", stdin) == NULL){
+        cout << "Error: data.txt not found!" << ln;
+        return 0;
+    }
+
     int n, m;
     cin >> n;
     for(int i=0; i<n; i++){
         int c;
-        cin >> c >> line[i];
+        cin >> c >> line[i]; 
     }
-    cin>>m;
+    cin >> m; // 간선 개수 입력
     for(int i=0; i<m; i++){
         int s, e, c;
         cin >> s >> e >> c;
-        map[s].push_back({e, c});
+        map[s].push_back({e, c}); 
     }
+
     int ans_now = ans;
-    for(starting_point = 0; starting_point<n; starting_point++){
-        cout << "now simulating:" << rp << starting_point << "\n";
+
+    // 시뮬레이션 시작
+    for(starting_point = 0; starting_point < n; starting_point++){
+        if(line[starting_point] != 0) continue; 
+
+        cout << "Simulating Start Point: " << starting_point << " (" << line[starting_point] << " line)" << ln;
+        
         memset(vis, 0, sizeof(vis));
-        memset(track, 0, sizeof(track));
-        bfs(starting_point);
+        
+        dijkstra(starting_point);
         if(ans_now != ans){
             ans_now = ans;
-            cout << ans << ln;
-            while(!(st.e == ans_starting_point && st.c == 1<<line[ans_starting_point])){
-                cout << st.e << rp;
-                st = track[st.e][st.c];
+            cout << "New Best Time: " << ans << ln;
+            
+            vector<int> path;
+            int curr_node = st.e; 
+            int curr_stat = st.c; 
+            
+            int trace_node = st.e;
+            int trace_stat = st.c;
+
+            cout << "Path (Reverse): ";
+            while(true){
+                cout << trace_node << " ";
+                path.push_back(trace_node);
+                
+                if(trace_node == ans_starting_point && trace_stat == (1<<line[ans_starting_point])){
+                    break;
+                }
+
+                Edge prev = track[trace_node][trace_stat];
+                trace_node = prev.e;
+                trace_stat = prev.c;
             }
-            cout << ans_starting_point<<ln;
+            cout << ln << "Starting Point Found!" << ln;
         }
     }
+    
+    cout << "Final Answer Time: " << ans << ln;
 }
